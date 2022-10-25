0f37deacee91401ff3743a5bbd342ecd5a11c25c
Tinton Arya Permana Sianturi <115549737+Sianturi1337@users.noreply.github.com>
2022-10-25 00:54:17 -0400

    Update maews_apache.sh
    Changing all design code.

--- tools/maews_apache.sh
+++ tools/maews_apache.sh
@@ -1,148 +1,158 @@
 #! /bin/bash
 
-red="\e[0;91m"
-blue="\e[0;94m"
-expand_bg="\e[K"
-blue_bg="\e[0;104m${expand_bg}"
-red_bg="\e[0;101m${expand_bg}"
-green_bg="\e[0;102m${expand_bg}"
-green="\e[0;92m"
-white="\e[0;97m"
-bold="\e[1m"
-uline="\e[4m"
-reset="\e[0m"
-start=${woro_woro}
+#### Colors Output
 
-sugeng_rawuh() {
-    echo -e "\n
-#####################################################################################
-# Welcome to MaeWS - WEB SERVER AUTO INSTALLER                                      #                    
-# MaeWS is based on APACHE WEB SERVER                                               #
-# LAMP is Linux, Apache, MySQL, PHP                                                 #
-# Description: with this tools, you can install Apache2 Web Server automatically    #
-# Author : Sianturi1337 - Tinton Arya Permana Sianturi                              #
-# Contact Me : sianturi[at]tintonarya.com                                           #
-#####################################################################################
-\n"
-}
-
-sugeng_rawuh
-
-iki_pilihanmu() {
-        echo -e "\n
-###############################################################################
-#              MaeWS - LAMP Edition (Linux, Apache, MySQL, PHP)               #
-###############################################################################    
-
-\n"
-}
-
-iki_pilihanmu
+RESET="\033[0m"			# Normal Colour
+RED="\033[0;31m" 		# Error / Issues
+GREEN="\033[0;32m"		# Successful       
+BOLD="\033[01;01m"    	# Highlight
+WHITE="\033[1;37m"		# BOLD
+YELLOW="\033[1;33m"		# WARNINGing
+PADDING="  "
+DPADDING="\t\t"
 
 
-warta_penting() {
-    echo -e "${red}${bold}[Important] : Make sure, you install MaeLAMP on a clean server or a new server !!!!\n${reset}"
-}
+#### Other Colors / Status Code
 
-warta_penting
+LGRAY="\033[0;37m"		# Light Gray
+LRED="\033[1;31m"		# Light Red
+LGREEN="\033[1;32m"		# Light GREEN
+LBLUE="\033[1;34m"		# Light Blue
+LPURPLE="\033[1;35m"	# Light Purple
+LCYAN="\033[1;36m"		# Light Cyan
+SORANGE="\033[0;33m"	# Standar Orange
+SBLUE="\033[0;34m"		# Standar Blue
+SPURPLE="\033[0;35m"	# Standar Purple      
+SCYAN="\033[0;36m"		# Standar Cyan
+DGRAY="\033[1;30m"		# Dark Gray
+TIME=$(date +'%H:%M:%S')
 
-iki_root_udu() {
+banner(){
 
-if [ "$EUID" -ne 0 ]; then
-  	echo -e "${red}${bold}[ MaeLAMP - Important Messages ] : Oops! You must run this tools by r00t \n${reset}"
-  exit
-fi
+    clear
+    echo -e "
+${GREEN}   __  ___       ${SPURPLE} _      ______${RESET}
+${GREEN}  /  |/  /__ ____${SPURPLE}| | /| / / __/${RESET}
+${GREEN} / /|_/ / _ '/ -_${SPURPLE}) |/ |/ /\ \  ${RESET}
+${GREEN}/_/  /_/\_,_/\__/${SPURPLE}|__/|__/___/ ${RESET}v{1.2#${YELLOW}dev${RESET}} by @Sianturi1337
+${SPURPLE}MaeWS${RESET} - ${SCYAN}LE${RESET}/${SCYAN}AMP${RESET} Automated installer\n\n"
 
 }
 
-iki_root_udu
+checkPrivileges(){
 
-ajian_detectos() {
-    echo -e "${blue_bg}${bold}\n[ MaeLEMP - OS Checker ] : Checking OS for Compability\n${reset}"; sleep 1;
-    mrintah="lsb_release -d"
-    metune=$(eval $mrintah 2>&1)
-    os=${metune//[[:blank:]]/}
-    os=${os#*:}
-    versine=$(eval "getconf LONG_BIT")
-    SingIso=("Ubuntu" "Debian")
-    if [[ " ${SingIso[@]} " =~ " ${os} " ]]; then
-        if hash mysql 2>/dev/null; then
-            echo -e "${red}${bold}[ MaeLAMP - MySQL ] : MySQL is already installed detected, this tools only supported for clean servers. \n${reset}"
-            echo -e "\n"
-        if hash apache2 2>/dev/null; then
-            echo -e "${red}${bold}[ MaeLAMP - Apache ] : Apache is already installed detected, this tools only supported for clean servers. \n${reset}"
-            echo -e "\n"
-        if hash php 2>/dev/null; then
-            echo -e "${red}${bold}[ MaeLAMP - PHP ] : PHP is already installed detected, this tools only supported for clean servers. \n${reset}"
-            echo -e "\n"
-            
-        else
-        ${start}
-            fi
+    echo "[*] start at $(date)"
+    echo -e "[*] ${GREEN}MaeWS${RESET} - ${SPURPLE}LAMP Edition${RESET} (${GREEN}Linux${RESET}, ${GREEN}Apache${RESET}, ${GREEN}MySQL${RESET}, ${GREEN}PHP${RESET})\n\n"
+    echo -e "[${LBLUE}${TIME}${RESET}] [${YELLOW}WARNING${RESET}] Make sure you install the ${GREEN}MaeLAMP${RESET} on a clean server${reset}"
+
+    if [ "$EUID" -ne 0 ]; then
+        echo -e "[${LBLUE}${TIME}${RESET}] [${RED}ERROR${RESET}] oops! You must run this tools using root/superuser privileges."
+    exit
+    fi
+
+}
+
+checkOS_package() {
+
+    echo -e "[${LBLUE}${TIME}${RESET}] [${GREEN}INFO${RESET}] Checking OS for Compability"
+    if [[ ${OSTYPE} == "linux-gnu" ]]; then
+        if [[ "$(cat /etc/os-release | grep PRETTY_NAME | cut -d '"' -f2 | awk '{print $1;}')" =~ ^(Ubuntu|Debian)$ ]]; then
+            if command -v mariadb &> /dev/null; then
+                echo -e "[${LBLUE}${TIME}${RESET}] [${LGREEN}INFO${RESET}] ${GREEN}MySQL${RESET} is already installed"
+            else
+                echo -e "[${LBLUE}${TIME}${RESET}] [${RED}ERROR${RESET}] ${RED}MySQL${RESET} is not installed, trying to install..."
+                echo -ne "[${LBLUE}${TIME}${RESET}] [${GREEN}INFO${RESET}] Updating repository ------->> "
+                `sudo apt -qq -y update &> /dev/null` #Updating repository with silent options
+                wait
+                echo -e "[${GREEN}DONE${RESET}]"
+                echo -ne "[${LBLUE}${TIME}${RESET}] [${GREEN}INFO${RESET}] Installing MySQL ------->> "
+                `sudo apt install mariadb-server mariadb-client -y -qq &> /dev/null`
+                echo -e "[${GREEN}DONE${RESET}]"
+                if [[ -z $(apt -qq list mariadb-server &> /dev/null) ]]; then
+                    echo -e "[${LBLUE}${TIME}${RESET}] [${RED}ERROR${RESET}] Failed to install MySQL (MariaDB), check manually required."
                 fi
             fi
+            if command -v apache2 &> /dev/null; then
+                echo -e "[${LBLUE}${TIME}${RESET}] [${LGREEN}INFO${RESET}] ${GREEN}Apache${RESET} is already installed"
+            else
+                echo -e "[${LBLUE}${TIME}${RESET}] [${RED}ERROR${RESET}] ${RED}Apache${RESET} is not installed, trying to install..."
+                echo -ne "[${LBLUE}${TIME}${RESET}] [${GREEN}INFO${RESET}] Updating repository ------->> "
+                `sudo apt -qq -y update &> /dev/null` #Updating repository with silent options
+                wait
+                echo -e "[${GREEN}DONE${RESET}]"
+                echo -ne "[${LBLUE}${TIME}${RESET}] [${GREEN}INFO${RESET}] Installing Apache2 ------->> "
+                `sudo apt install apache2 -y -qq &> /dev/null`
+                echo -e "[${GREEN}DONE${RESET}]"
+                if [[ -z $(apt -qq list apache2 &> /dev/null) ]]; then
+                    echo -e "[${LBLUE}${TIME}${RESET}] [${RED}ERROR${RESET}] Failed to install Apache2, check manually required."
+                fi
+            fi
+            if command -v php &> /dev/null; then
+                echo -e "[${LBLUE}${TIME}${RESET}] [${LGREEN}INFO${RESET}] ${GREEN}PHP${RESET} is already installed"
+            else
+                echo -e "[${LBLUE}${TIME}${RESET}] [${RED}ERROR${RESET}] ${RED}PHP${RESET} is not installed, trying to install..."
+                echo -ne "[${LBLUE}${TIME}${RESET}] [${GREEN}INFO${RESET}] Installing APT libraries ------->> "
+                `sudo apt install lsb-release ca-certificates apt-transport-https software-properties-common -y -qq &> /dev/null`
+                echo -e "[${GREEN}DONE${RESET}]"
+                echo -ne "[${LBLUE}${TIME}${RESET}] [${GREEN}INFO${RESET}] Adding ${GREEN}ppa:ondrej/php${RESET} to repository "
+                `add-apt-repository -y ppa:ondrej/php > /dev/null 2>&1`
+                echo -e "[${GREEN}DONE${RESET}]"
+                echo -ne "[${LBLUE}${TIME}${RESET}] [${GREEN}INFO${RESET}] Updating repository ------->> "
+                `sudo apt -qq -y update &> /dev/null` #Updating repository with silent options
+                wait
+                echo -e "[${GREEN}DONE${RESET}]"
+                echo -ne "[${LBLUE}${TIME}${RESET}] [${GREEN}INFO${RESET}] Installing PHP ------->> "
+                `sudo apt install php-fpm php-mysql php-cli php-zip php-mbstring php-imap php-common php-redis php-xml -y -qq &> /dev/null`
+                echo -e "[${GREEN}DONE${RESET}]"
+                if [[ -z $(apt -qq list php 2>/dev/null) ]]; then
+                    echo -e "[${LBLUE}${TIME}${RESET}] [${RED}ERROR${RESET}] Failed to install PHP, check manually required."
+                fi
+            fi
+                    
+        else
+            echo -e "[${LBLUE}${TIME}${RESET}] [${LRED}ERROR${RESET}] OS not compatible, Your Operating System is ${GREEN}${OSTYPE}${RESET}"
+            echo -e "[${LBLUE}${TIME}${RESET}] [${GREEN}INFO${RESET}] Supported OS ${GREEN}Debian${RESET} or ${GREEN}Ubuntu${RESET} (linux-gnu)"
+            exit
         fi
+    else
+        echo -e "[${LBLUE}${TIME}${RESET}] [${LRED}ERROR${RESET}] OS not compatible, Your Operating System is ${GREEN}${OSTYPE}${RESET}"
+        echo -e "[${LBLUE}${TIME}${RESET}] [${GREEN}INFO${RESET}] Supported OS ${GREEN}Debian${RESET} or ${GREEN}Ubuntu${RESET} (linux-gnu)"
+        exit
+    fi
 
 }
 
-ajian_detectos
+checkService(){
 
-echo -e "\n"
+    echo -ne "[${LBLUE}${TIME}${RESET}] [${GREEN}INFO${RESET}] Enabling Apache2 service ------->> "
+    `systemctl enable apache2 &>/dev/null && systemctl restart apache2 &>/dev/null`
+    wait
+    echo -e "[${GREEN}DONE${RESET}]"
+    echo -ne "[${LBLUE}${TIME}${RESET}] [${GREEN}INFO${RESET}] Apache2 service status ------->> "
+    if [[ $(systemctl is-active apache2) == "active" ]]; then
+        echo -e "[${GREEN}ACTIVE${RESET}]"
+    else
+        echo -e "[${LRED}ERROR${RESET}]"
+        echo -e "[${LBLUE}${TIME}${RESET}] [${RED}ERROR${RESET}] Failed to start Apache2, check manually required"
+    fi
+    echo -ne "[${LBLUE}${TIME}${RESET}] [${GREEN}INFO${RESET}] Enabling MySQL (MariaDB) service ------->> "
+    `systemctl enable mysql &>/dev/null && systemctl restart mysql &>/dev/null`
+    wait
+    echo -e "[${GREEN}DONE${RESET}]"
+    echo -ne "[${LBLUE}${TIME}${RESET}] [${GREEN}INFO${RESET}] MySQL (MariaDB) service status ------->> "
+    if [[ $(systemctl is-active mysql) == "active" ]]; then
+        echo -e "[${GREEN}ACTIVE${RESET}]"
+    else
+        echo -e "[${LRED}ERROR${RESET}]"
+        echo -e "[${LBLUE}${TIME}${RESET}] [${RED}ERROR${RESET}] Failed to start MySQL (MariaDB), check manually required"
+    fi
 
-woro_woro() {
-    for i in `seq 3 -1 1` ; do echo -ne "$i\rWelcome to MaeLAMP AUTO INSTALLER will be start in . . . . "; sleep 1; done 
+    echo "[*] ended at $(date)"
 }
 
-woro_woro
 
-echo -e "\n"
-
-nganyari_sistem() {
-    echo -e "${blue_bg}${bold}\n[ MaeLAMP - Updating ] : The system is currently updating and upgrading your server to make performance better . . . \n${reset}";
-    sudo apt update -y && sudo apt upgrade -y || echo -e "${red_bg}${bold}\n[ERROR] : We can't update and upgrading your server, please check.\n${reset}"
-}
-
-nganyari_sistem
-
-nambahi_repo() {
-    echo -e "${blue_bg}${bold}\n [ MaeLEMP - Adding Repository ] : Adding ppa:ondrej/php to your server\n${reset}"; sleep 1;
-    apt install lsb-release ca-certificates apt-transport-https software-properties-common -y
-    add-apt-repository ppa:ondrej/php -y
-    sudo apt update -y && sudo apt upgrade -y
-}
-
-nambahi_repo
-
-masangi_alat() {
-        echo -e "${blue_bg}${bold}\n[ MaeLAMP - Install Apache ] : Installing Apache .... \n${reset}";
-        apt install apache2 -y || echo -e "${red_bg}${bold}\n[ERROR] : We can't installing Apache on your server, please check.\n${reset}"
-        
-        echo -e "${blue_bg}${bold}\n[ MaeLAMP - Enabling Apache ] : Enabling and Restarting Apache .... \n${reset}";
-        systemctl enable apache2 && systemctl restart apache2
-        echo -e "${blue_bg}${bold}\n[ MaeLAMP - Install MySQL Database Server ] : Installing MariaDB Server & Client .... \n${reset}";
-        apt install mariadb-server mariadb-client -y || echo -e "${red_bg}${bold}\n[ERROR] : We can't installing MariaDB Server & Client on your server, please check.\n${reset}"
-
-        echo -e "${blue_bg}${bold}\n[ MaeLAMP - Enabling MySQL Database Server ] : Enabling and Restarting MariaDB .... \n${reset}";
-        systemctl enable mysql && systemctl restart mysql
-        echo -e "\n"
-        echo -e "${blue_bg}${bold}\n[ MaeLAMP - Install PHP ] : Installing PHP to your Server\n${reset}";
-        apt install php8.0-fpm php-mysql php8.0-cli php8.0-zip php8.0-mbstring php8.0-imap php8.0-common php8.0-redis php8.0-xml -y || echo -e "${red_bg}${bold}\n[ERROR] : We can't installing PHP in your server, please check.\n${reset}"
-        ${matur_suwun}
-}
-
-masangi_alat
-
-matur_suwun() {
-    echo -e "${green}${bold}
-=====================================================================
-|                            THANK YOU!                             |
-|                YOUR LEMP STACK HAS BEEN INSTALLED                 |
-|                                BY                                 |
-|                       MaeLEMP AUTO INSTALLER                      |
-=====================================================================
-\n${reset}"
-
-}
-
-matur_suwun
+banner
+checkPrivileges
+checkOS_package
+checkService
 exit
